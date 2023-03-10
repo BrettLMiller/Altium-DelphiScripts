c4fcaf5782e42a65ef3932a454b7310d7ebe01c9
BrettLMiller <54195045+BrettLMiller@users.noreply.github.com>
2023-03-11 09:19:41 +1300

    Create StartAndReturn.bat
    POC

---
+++ System/DialogClose/StartAndReturn.bat
@@ -0,0 +1,6 @@
+Rem ' StartAndReturn.bat
+Rem ' full path may not be required (same folder) in MS-Windows.
+
+start /b wscript "C:\Altium Projects\Scripts\System\DialogClose\FocusAltium.vbs" %1
+
+rem echo %1 > c:\temp\cmdpara.txt
