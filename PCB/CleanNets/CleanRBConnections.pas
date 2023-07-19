715ac3288555189162d5ec7dd39ec500be3bb4ed
BrettLMiller <54195045+BrettLMiller@users.noreply.github.com>
2023-07-19 16:53:07 +1200

    Add files via upload
    Fix TCoord maths magnitude bug. Improve connection refresh timing (just after moveXY)

--- PCB/CleanNets/CleanRBConnections.pas
+++ PCB/CleanNets/CleanRBConnections.pas
@@ -2,11 +2,11 @@
 
 Run CleanNets() for all connection objects.
 
-Sometimes moving rooms & groups of components leaves unresolved 
+Sometimes moving rooms & groups of components leaves unresolved
 connections (rubberbands)
 
 <SHIFT>  move locked components
-<CTRL>   extended selection.
+<CTRL>   extended selection: leave moved FP selected, allow group move.
 
 <ALT>   is not available with ChooseLocation()   check this is consistent AD17 & AD21
 
@@ -14,7 +14,7 @@ B. Miller
 12/07/2019 : v0.1  Initial POC
 04/09/2019 : v0.2  Fix modifying Connections inside iterator loop!
 29/07/2022 : v0.21 fix modifier keys & add "locked" support.
-
+2023-07-19 : v0.22 fix TCoord maths problems.
 }
 const
     cESC      =-1;
@@ -22,25 +22,26 @@ const
     cShiftKey = 2;
     cCntlKey  = 3;
 
+    cDebugF   = false;
+
 var
     Board  : IPCB_Board;
     Keyset : TSet;
-
-procedure CleanNetsProcessCall;
-begin
-    Client.SendMessage('PCB:Netlist', 'Action=CleanUpNets|Prompt=false' , 255, Client.CurrentView);
-end;
+    Report : TStringList;
 
 Procedure CleanUpNetConnections(Board : IPCB_Board);
 var
     Connect  : IPCB_Connection;
     Iterator : IPCB_BoardIterator;
     Net      : IPCB_Net;
-    NetList  : TObjectList;
+    NetList  : TStringList;
     N        : integer;
 
 begin
-    NetList := TObjectList.Create;
+    NetList := TStringList.Create;
+    NetList.Sorted := true;
+    NetList.Duplicates := dupIgnore;
+
     Iterator := Board.BoardIterator_Create;
     Iterator.AddFilter_ObjectSet(MkSet(eConnectionObject));
     Iterator.AddFilter_LayerSet(AllLayers);
@@ -51,8 +52,7 @@ begin
     begin
         Net := Connect.Net;
         if Net <> Nil then
-            if NetList.IndexOf(Net) = -1 then
-                NetList.Add(Net);
+            NetList.AddObject(Net.Name, Net);
 
         Connect := Iterator.NextPCBObject;
     end;
@@ -60,26 +60,34 @@ begin
 
     for N := 0 to (NetList.Count - 1) do
     begin
-        Net := NetList.Items(N);
+        Net := NetList.Objects(N);
         Board.CleanNet(Net);
     end;
-    NetList.Destroy;
+    NetList.Free;
 end;
 
 function TestInsideBoard(Component : IPCB_Component) : boolean;
 // for speed.. any part of comp touches or inside BO
 var
+    BOL    : IPCB_BoardOutline;
     Prim   : IPCB_Primitive;
     PIter  : IPCB_GroupIterator;
 begin
+
+    BOL := Board.BoardOutline;
+//    Result := BOL.PrimitiveInsidePoly(Component);
     Result := false;
     PIter  := Component.GroupIterator_Create;
     PIter.AddFilter_ObjectSet(MkSet(ePadObject, eRegionObject));
     Prim := PIter.FirstPCBObject;
-    While (Prim <> Nil) and (not Result) Do
+    While (Prim <> Nil) Do
     Begin
         if Board.BoardOutline.GetState_HitPrimitive (Prim) then
+//        if BOL.PrimitiveInsidePoly(Prim) then
+        begin
             Result := true;
+            break;
+        end;
         Prim := PIter.NextPCBObject;
     End;
     Component.GroupIterator_Destroy(PIter);
@@ -87,14 +95,15 @@ end;
 
 function CleanCompNets(FP : IPCB_Component) : boolean;
 var
-    Prim       : IPCB_Primitive;
+    Prim     : IPCB_Primitive;
     Net      : IPCB_Net;
-    NetList  : TObjectList;
+    NetList  : TStringList;
     N, i     : integer;
 begin
     Result := false;
-    NetList := TObjectList.Create;
-    NetList.OwnsObjects := false;
+    NetList := TStringList.Create;
+    NetList.Sorted := true;
+    NetList.Duplicates := dupIgnore;
 
     if FP.ObjectId = eComponentObject then
         for i := 1 to FP.GetPrimitiveCount(MkSet(ePadObject) ) do
@@ -102,35 +111,33 @@ begin
             Prim := FP.GetPrimitiveAt(i, ePadObject);
             Net := Prim.Net;
             if Net <> Nil then
-                if NetList.IndexOf(Net) = -1 then
-                    NetList.Add(Net);
+                NetList.AddObject(Net.Name, Net);
         end
     else
         if FP.InNet then
         begin
-            Net := Prim.Net;
-                if Net <> Nil then
-                    if NetList.IndexOf(Net) = -1 then
-                        NetList.Add(Net);
+            Net := FP.Net;
+            if Net <> Nil then
+                NetList.AddObject(Net.Name, Net);
         end;
 
     for N := 0 to (NetList.Count - 1) do
     begin
-        Net := NetList.Items(N);
+        Net := NetList.Objects(N);
         Board.CleanNet(Net);
         Result := true;
     end;
-    NetList.Destroy;
+    NetList.Free;
 end;
 
-function GetComponentAtXY(ObjectSet : TObjectSet, X,Y : TCoord) : IPCB_Component;
+function GetComponentAtXY(ObjectSet : TObjectSet, const X, const Y : TCoord) : IPCB_Component;
 var
     SIterator  : IPCB_SpatialIterator;
     Prim       : IPCB_Primitive;
     Distance : TCoord;
 begin
     Result := nil;
-    Distance := 100;
+    Distance := 1000;
     SIterator := Board.SpatialIterator_Create;
     SIterator.SetState_FilterAll;
     SIterator.AddFilter_LayerSet(MkSet(eTopLayer, eBottomLayer));
@@ -156,30 +163,32 @@ begin
     if not Prim.Moveable then
     if not InSet(cShiftKey, KeySet) then exit;
 
-    PCBServer.SendMessageToRobots(Prim.I_ObjectAddress, c_Broadcast, PCBM_BeginModify , c_NoEventData);
     Prim.BeginModify;
     Prim.MoveToXY(x,y);
     Prim.EndModify;
-    PCBServer.SendMessageToRobots(Prim.I_ObjectAddress, c_Broadcast, PCBM_EndModify , c_NoEventData);
-
-    Prim.SetState_XSizeYSize;
     Board.ViewManager_GraphicallyInvalidatePrimitive(Prim);
-//    Prim.GraphicallyInvalidate;
+
+    CleanCompNets(Prim);
 
     if not InSet(cCntlKey, KeySet) then
         Client.SendMessage('PCB:DeSelect','Scope=All',255,Client.CurrentView);
 
     Prim.Selected := true;
-    Board.AddObjectToHighlightObjectList(Prim);
-    CleanCompNets(Prim);
+//    Board.AddObjectToHighlightObjectList(Prim);
 
     Client.SendMessage('PCB:MoveObject','Drag=True|Object=Selection|ContextObject=Component',255, Client.CurrentView);
+
+// try these to get on cursor.
+//    ResetParameters;
+//    AddStringParameter('RepositionSelected', 'True');
+//    RunProcess('PCB:PlaceComponent');
 end;
 
 function GreaterMagnitude(FP1 : IPCB_Primitive, FP2 : IPCB_Primitive, x : TCoord, y : TCoord) : IPCB_Primitive;
 var
     SMag1, SMag2 : double;
     BR1, BR2     : TRect;
+    CP1, CP2     : TCoordPoint;
 begin
     Result := FP1;
     if Result = nil then
@@ -187,50 +196,20 @@ begin
         Result := FP2;
         exit;
     end;
-    BR1 := FP1.BoundingRectangle;
-    BR2 := FP2.BoundingRectangle;
+    BR1 := FP1.BoundingRectangleForSelection;
+    BR2 := FP2.BoundingRectangleForSelection;
+    CP1 := Point(BR1.X1 + RectWidth(BR1)/2, BR1.Y1 + RectHeight(BR1)/2);
+    CP2 := Point(BR2.X1 + RectWidth(BR2)/2, BR2.Y2 + RectHeight(BR2)/2);
 
-    SMag1 :=            (x - BR1.X1)*(x - BR1.X1) + (y - BR1.Y1)*(y - BR1.Y1);
-    SMag1 := Min(SMag1, (x - BR1.X2)*(x - BR1.X2) + (y - BR1.Y1)*(y - BR1.Y1) );
-    SMag1 := Min(SMag1, (x - BR1.X2)*(x - BR1.X2) + (y - BR1.Y2)*(y - BR1.Y2) );
-    SMag1 := Min(SMag1, (x - BR1.X1)*(x - BR1.X1) + (y - BR1.Y2)*(y - BR1.Y2) );
+    SMag1 := Power((x - CP1.X), 2) + Power((y - CP1.Y), 2);
+    SMag2 := Power((x - CP2.X), 2) + Power((y - CP2.Y), 2);
+    SMag1 := Sqrt(SMag1);
+    SMag2 := Sqrt(SMag2);
 
-    SMag2 :=            (x - BR2.X1)*(x - BR2.X1) + (y - BR2.Y1)*(y - BR2.Y1);
-    SMag2 := Min(SMag2, (x - BR2.X2)*(x - BR2.X2) + (y - BR2.Y1)*(y - BR2.Y1) );
-    SMag2 := Min(SMag2, (x - BR2.X2)*(x - BR2.X2) + (y - BR2.Y2)*(y - BR2.Y2) );
-    SMag2 := Min(SMag2, (x - BR2.X1)*(x - BR2.X1) + (y - BR2.Y2)*(y - BR2.Y2) );
+    Report.Add('FP1:' + FloatValueToString(SMag1, eMil) + '  FP2:' + FloatValueToString(SMag2, eMil));
     if SMag2 > SMag1 then Result := FP2;
 end;
 
-
-function GetStatusBar : WideString;
-var
-    GUIMan       : IGUIManager;
-    ClientModule : IClient;
-    S            : WideString;
-    Focus        : WideString;
-
-begin
-    ClientModule := Client;
-    If ClientModule = Nil Then Exit;
-    GUIMan := ClientModule.GUIManager;
-    If GUIMan = Nil Then Exit;
-
-    GUIMan.StatusBar_GetState(0,Text);
-    Result := GUIMan.StatusBar_GetState(0);
-//    Focus := GUIMan.GetFocusedPanelName;
-end;
-Procedure SetStatusBar(Text : WideString);
-var
-    GUIMan       : IGUIManager;
-    ClientModule : IClient;
-begin
-    ClientModule := Client;
-    If ClientModule = Nil Then Exit;
-    GUIMan := ClientModule.GUIManager;
-    If GUIMan = Nil Then Exit;
-    GUIMan.StatusBar_SetState(0,Text);
-end;
 
 Procedure LassoCompByConnections;
 var
@@ -242,6 +221,7 @@ var
     LayerSet    : IPCB_LayerSet;
     Prim        : IPCB_Primitive;
     x, y        : TCoord;
+    NM1, NM2    : WideString;
     ObjectSet   : TObjectSet;
     msg         : WideString;
     DoAgain   : boolean;
@@ -254,12 +234,15 @@ begin
     Board := PCBServer.GetCurrentPCBBoard;
     If Board = Nil Then Exit;
     Grid := Board.SnapGridSize;
+    Grid := 1000;
+
+    Report := TStringList.Create;
 
     Client.SendMessage('PCB:DeSelect','Scope=All',255,Client.CurrentView);
 
 //    TV6_LayerSet := AllLayers;
     ObjectSet := MkSet(eConnectionObject);
-    LayerSet := LayerSetUtils.EmptySet.Include(eConnectLayer);
+    LayerSet := LayerSetUtils.CreateLayerSet.Include(eConnectLayer);
 
     msg := 'Pick your connection (ESC exit)';
 
@@ -278,7 +261,7 @@ begin
            SIterator.SetState_FilterAll;
            SIterator.AddFilter_IPCB_LayerSet(LayerSet);  // (eConnectLayer);
            SIterator.AddFilter_ObjectSet(ObjectSet);
-           SIterator.AddFilter_Area(X - Grid, Y - Grid, X + Grid, Y + Grid);
+           SIterator.AddFilter_Area(X - Grid/2, Y - Grid/2, X + Grid/2, Y + Grid/2);
            Prim := SIterator.FirstPCBObject;
 
            while Prim <> Nil do
@@ -300,13 +283,16 @@ begin
                 Connect.Selected := true;
                 Net := Connect.Net;
                 if Net <> Nil then
-//                    ShowMessage('net name : '+ Net.Name);
-                    SetStatusBar((Net.Name));
- //               ShowMessage('X:' + IntToStr(Connect.X1) +  ' Y:' +IntToStr(Connect.Y1));
+                    Report.Add('X:' + CoordUnitToString(Connect.X1, eMil) +  ' Y:' + CoordUnitToString(Connect.Y1, eMil) );
             end;
 
-            FP1 := GetComponentAtXY(MkSet(ePadObject, eComponentObject, eRegionObject), Connect.X1, Connect.Y1);
-            FP2 := GetComponentAtXY(MkSet(ePadObject, eComponentObject, eRegionObject), Connect.X2, Connect.Y2);
+            FP1 := GetComponentAtXY(MkSet(ePadObject), Connect.X1, Connect.Y1);
+            FP2 := GetComponentAtXY(MkSet(ePadObject), Connect.X2, Connect.Y2);
+
+            NM1 := 'no FP'; NM2 := NM1;
+            if FP1 <> nil then NM1 := FP1.Name.Text;
+            if FP2 <> nil then NM2 := FP2.Name.Text;
+            Report.Add('FP1:' + NM1 + '  FP2:' + NM2);
 
             FP1Inside := false;
             if FP1 <> nil then
@@ -340,6 +326,9 @@ begin
             DoAgain := false;
 
     until not(DoAgain);
+
+    if cDebugF then
+        Report.SaveToFile('c:\temp\debug.txt');
 end;
 
 procedure RubberBandMan;
@@ -355,6 +344,11 @@ begin
     Board.GraphicallyInvalidate;
 
     EndHourGlass;
-
-    Client.SendMessage('PCB:Zoom', 'Action=Redraw', 1024, Client.CurrentView);
+    Board.GraphicalView_ZoomRedraw;
 end;
+
+procedure CleanNetsProcessCall;
+begin
+    Client.SendMessage('PCB:Netlist', 'Action=CleanUpNets|Prompt=false' , 255, Client.CurrentView);
+end;
+
