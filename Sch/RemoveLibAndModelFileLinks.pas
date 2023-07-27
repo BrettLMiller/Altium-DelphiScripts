332a5053a574b224ce887b35086bad7cbff8c904
BrettLMiller <54195045+BrettLMiller@users.noreply.github.com>
2023-07-27 16:55:25 +1200

    Add files via upload
    add support to wipe the parameters & models from a whole folder of SchLibs

--- Sch/RemoveLibAndModelFileLinks.pas
+++ Sch/RemoveLibAndModelFileLinks.pas
@@ -21,9 +21,8 @@ This causes problem making SchLib from these Comps.
  28/02/2020 v1.2  Overwrite .SymbolReference so DBLib sourced comps can be made into SchLib.
  19/10/2020 v1.3  Added function to completely delete the model SchImp. to make Components into Symbols
  2023-07-27 v1.4  Full Wipeout: remove all Comp parameters & models. Set Comment & Desc.
+ 2023-07-27 v1.5  fix SchLib Cmp model & panel refreshing.
 
-
-tbd: SchLib still needs Comp navigation to refresh SchImp models display
 ..................................................................................}
 
 {..............................................................................}
@@ -37,56 +36,75 @@ Var
     dConfirm    : boolean;
 
 Function GetAllSchCompParameters(const Component : ISch_BasicContainer) : TList; forward;
+Procedure TotalWipeoutComponentsToSymbols(const CurrentLib : ISch_Lib); forward;
+Procedure GenerateReport(Report : TStringList, Filename : WideString);  forward;
+Procedure SetDocumentDirty (Dummy : Boolean); forward;
 
-Procedure GenerateReport(Report : TStringList, Filename : WideString);
-Var
+procedure WipeAllSchLibsInFolder();
+var
+    ServerDoc  : IServerDocument;
+    LibList    : TStringList;
+    Path       : WideString;
+    FileName   : WideString;
+    SchLib     : ISch_Lib;
+    I          : integer;
+begin
+    LibList := TStringList.Create;
+    ResetParameters;
+    AddStringParameter('Dialog',    'FileOpenSave');
+    AddStringParameter('Mode',      '0');
+    AddStringParameter('FileType1', 'SchLib File (*.SchLib)|*.SchLib');
+    AddStringParameter('Prompt',    'Select SchLib in target folder ');
+    AddStringParameter('Path',      Path);
+    RunProcess('Client:RunCommonDialog');
 
-    ReportDocument : IServerDocument;
-    Filepath       : WideString;
-
-Begin    
-    WS  := GetWorkspace;
-    If WS <> Nil Then
+    GetStringParameter('Result', Path);
+    if Path = 'True' then
     begin
-       Prj := WS.DM_FocusedProject;
-
-       Doc := WS.DM_FocusedDocument;
-
-       If Prj <> Nil Then
-          Filepath := ExtractFilePath(Prj.DM_ProjectFullPath);   //  Doc.DM_FullPath
-
-//  to get unique report file per SchLib doc..
-    //   Filepath := ExtractFilePath(Doc.FullPath) + ChangefileExt(ExtractFileName(Doc.FullPath), '');
-
+        GetStringParameter('Path',   Path);
+        Path     := ExtractFilePath(Path);
+        FileName := ExtractFileName(Path);
+// mode 2 & 4 not supported
+//        I := 1;
+//        repeat
+//            GetStringParameter('File'+IntToStr(I),  FileName);
+//            LibList.Add(FileName);
+//            inc(I);
+//        until FileName = '';
     end;
 
-    If length(Filepath) < 5 then Filepath := ExtractFilePath(Doc.DM_FullPath);
- 
-    Filepath := Filepath + Filename;
+    dConfirm := ConfirmNoYesWithCaption('Ready to Totally Remove ALL models in all SchLibs in Whole Folder ','Do you really want to do this ? ');
+    if not dconfirm then exit;
+    dConfirm := ConfirmNoYesWithCaption('Totally Eliminate/Remove ALL models from all CMps in All Schibs (make symbols) ','Very sure ? ');
+    if not dconfirm then exit;
 
-    ModelsList.Insert(0, ExtractFilename(Prj.DM_ProjectFullPath));
-    Report.SaveToFile(Filepath);
+// all stupid capitalised text
+    FindFiles(Path, '*.SchLib',faAnyFile, false, LibList);
 
-    ReportDocument := Client.OpenDocument('Text',Filepath);
-    If ReportDocument <> Nil Then
+    Modelslist := TStringList.Create;
+    ModelsList.Add    ('Remove All Existing Parameters/FP models in folder : ' + Path);
+    ModelsList.Add('');
+
+    for I := 0 to (LibList.Count - 1) do
     begin
-        Client.ShowDocument(ReportDocument);
-        if (ReportDocument.GetIsShown <> 0 ) then
-            ReportDocument.DoFileLoad;
+        Filename := LibList.Strings(I);
+
+// without this will process but not save back to filesystem!
+        ServerDoc  := Client.OpenDocumentShowOrHide(cDocKind_Schlib, Filename, false);
+
+        SchLib := SchServer.GetSchDocumentByPath(Filename);
+        if SchLib = nil then
+            SchLib := SchServer.LoadSchDocumentByPath(Filename);
+        if SchLib <> nil then
+        begin
+            TotalWipeoutComponentsToSymbols(SchLib);
+            ServerDoc.DoSafeFileSave(cDocKind_Schlib)
+        end;
     end;
+
+    GenerateReport(ModelsList, 'WipedCMPinSchLibFolder.txt');
+    Modelslist.Free;
 end;
-
-Procedure SetDocumentDirty (Dummy : Boolean);
-Var
-    AView           : IServerDocumentView;
-    AServerDocument : IServerDocument;
-Begin
-    If Client = Nil Then Exit;
-    AView := Client.GetCurrentView;
-    AServerDocument := AView.OwnerDocument;
-    AServerDocument.Modified := True;
-End;
-{..............................................................................}
 
 {..............................................................................}
 Procedure RemoveLibraryPathFromSchComps;
@@ -213,7 +231,6 @@ Begin
 
 End;
 
-
 Procedure RemoveFPModelDataFileLinksFromLibComps;
 Var
     CurrentLib         : ISch_Lib;
@@ -249,7 +266,7 @@ Begin
     Modelslist := TStringList.Create;
     ModelsList.Add    (' All Existing FP models : ' + '  for datafile like removal/deleting. ');
     ModelsList.Add('');
-    
+
     Try
         Component := Iterator.FirstSchObject;
 
@@ -303,8 +320,36 @@ Begin
 End;
 
 Procedure TotallyWipeoutMyComponentsToSymbols;
+var
+    CurrentLib : ISch_Lib;
+begin
+    If SchServer = Nil Then Exit;
+    CurrentLib := SchServer.GetCurrentSchDocument;
+    If CurrentLib = Nil Then Exit;
+
+    If (CurrentLib.ObjectID <> eSchLib) Then
+    Begin
+         ShowError('Operates on SchLib only.');
+         Exit;
+    End;
+
+    dConfirm := ConfirmNoYesWithCaption('Ready to Totally Remove ALL models ','Do you really want to do this ? ');
+    if not dconfirm then exit;
+    dConfirm := ConfirmNoYesWithCaption('Totally Eliminate/Remove ALL models from components (make into symbols) ','Very sure ? ');
+    if not dconfirm then exit;
+
+    Modelslist := TStringList.Create;
+    ModelsList.Add    (' All Existing FP : ' + '  for datafile like removal/deleting. ');
+    ModelsList.Add('');
+
+    TotalWipeoutComponentsToSymbols(CurrentLib);
+
+    GenerateReport(ModelsList, 'DataFileLinksRemovedFromCompLib.txt');
+    Modelslist.Free;
+end;
+
+Procedure TotalWipeoutComponentsToSymbols(const CurrentLib : ISch_Lib);
 Var
-    CurrentLib         : ISch_Lib;
     Iterator           : ISch_Iterator;
     Component          : ISch_Component;
     ImplIterator       : ISch_Iterator;
@@ -318,100 +363,80 @@ Var
 Begin
     FLinkCount := 0;
 
-    If SchServer = Nil Then Exit;
-    CurrentLib := SchServer.GetCurrentSchDocument;
-    If CurrentLib = Nil Then Exit;
-
-    If (CurrentLib.ObjectID <> eSchLib) Then
-    Begin
-         ShowError('Operates on SchLib only.');
-         Exit;
-    End;
-//    If PcbServer = Nil then Exit;
+    ModelsList.Add(CurrentLib.DocumentName);
 
     Iterator := CurrentLib.SchLibIterator_Create;
     Iterator.AddFilter_ObjectSet(MkSet(eSchComponent));
-
-    dConfirm := ConfirmNoYesWithCaption('Ready to Totally Remove ALL models ','Do you really want to do this ? ');
-    if not dconfirm then exit;
-    dConfirm := ConfirmNoYesWithCaption('Totally Eliminate/Remove ALL models from components (make into symbols) ','Very sure ? ');
-    if not dconfirm then exit;
-
-    Modelslist := TStringList.Create;
-    ModelsList.Add    (' All Existing FP : ' + '  for datafile like removal/deleting. ');
-    ModelsList.Add('');
     
     Component := Iterator.FirstSchObject;
-
     While Component <> Nil Do
     Begin
-            ModelsList.Add (' Comp LibRef : ' + Component.LibReference);
-            FPCount := 0;
+        ModelsList.Add (' Comp LibRef : ' + Component.LibReference);
+        FPCount := 0;
 
-            Component.ComponentDescription := 'SYM';
-            Component.Comment.Text := Component.LibReference;
+        SchServer.RobotManager.SendMessage(Component.I_ObjectAddress, c_BroadCast, SCHM_BeginModify, c_NoEventData);
 
-            ParasList := GetAllSchCompParameters(Component);
+        Component.ComponentDescription := 'SYM';
+        Component.Comment.Text := Component.LibReference;
+
+        ParasList := GetAllSchCompParameters(Component);
+        for i := 0 to (ParasList.Count - 1) do
+        begin
+            Parameter := ParasList.Items(i);
+            if Parameter.Name = 'Comment' then continue;
+
+            Component.Remove_Parameter(Parameter);
+            Component.RemoveSchObject(Parameter);
+            CurrentLib.UnRegisterSchObjectFromContainer(Parameter);
+            CurrentLib.RemoveSchObject(Parameter);
+//            SchServer.DestroySchObject(Parameter);
+        end;
+        ParasList.Free;
+
+        ImplIterator := Component.SchIterator_Create;
+        ImplIterator.AddFilter_ObjectSet(MkSet(eImplementation, eImplementationMap, eImplementationsList));
+
+        SchImplementation := ImplIterator.FirstSchObject;
+        While SchImplementation <> Nil Do
+        Begin
+            Inc(FPCount);
+
+            ParasList := GetAllSchCompParameters(SchImplementation);
             for i := 0 to (ParasList.Count - 1) do
             begin
                 Parameter := ParasList.Items(i);
-                if Parameter.Name = 'Comment' then continue;
+                Parameter.Name;
+
+                SchImplementation.RemoveSchObject(Parameter);
                 Component.Remove_Parameter(Parameter);
-                Component.RemoveSchObject(Parameter);
                 CurrentLib.UnRegisterSchObjectFromContainer(Parameter);
                 CurrentLib.RemoveSchObject(Parameter);
-//                SchServer.DestroySchObject(Parameter);
             end;
             ParasList.Free;
 
-
-            ImplIterator := Component.SchIterator_Create;
-            ImplIterator.AddFilter_ObjectSet(MkSet(eImplementation, eImplementationMap, eImplementationsList));
-
-                SchImplementation := ImplIterator.FirstSchObject;
-                While SchImplementation <> Nil Do
-                Begin
-                    Inc(FPCount);
-
-                    ParasList := GetAllSchCompParameters(SchImplementation);
-                    for i := 0 to (ParasList.Count - 1) do
-                    begin
-                        Parameter := ParasList.Items(i);
-//                      if Parameter.Name = 'Comment' then continue;
-                        SchImplementation.Remove_Parameter(Parameter);
-                        SchImplementation.RemoveSchObject(Parameter);
-                        CurrentLib.UnRegisterSchObjectFromContainer(Parameter);
-                        CurrentLib.RemoveSchObject(Parameter);
-//                SchServer.DestroySchObject(Parameter);
-                    end;
-                    ParasList.Free;
-
-
 //      remove all existing footprint models
-                   if SchImplementation.ObjectID = eImplementation then
-                   begin
-                       ModelsList.Add    ('     Deleted Existing model datafile link : ' + SchImplementation.ModelName);
-                       SchImplementation.ClearAllDatafileLinks;
-                   end;
+            if SchImplementation.ObjectID = eImplementation then
+            begin
+               ModelsList.Add    ('     Deleted Existing model datafile link : ' + SchImplementation.ModelName);
+               SchImplementation.ClearAllDatafileLinks;
+            end;
 
-                   Inc(FLinkCount);
+            Inc(FLinkCount);
 
 // danger this completely removes the models.
-                    Component.RemoveSchImplementation(SchImplementation);
-                    Component.RemoveSchObject(SchImplementation);
-                    CurrentLib.UnRegisterSchObjectFromContainer(SchImplementation);
-                    CurrentLib.RemoveSchObject(SchImplementation);
-//                    SchServer.DestroySchObject(SchImplementation);
+            Component.RemoveSchImplementation(SchImplementation);
+            Component.RemoveSchObject(SchImplementation);
+            CurrentLib.UnRegisterSchObjectFromContainer(SchImplementation);
+            CurrentLib.RemoveSchObject(SchImplementation);
 
-                    SchImplementation := ImplIterator.NextSchObject;
+            SchImplementation := ImplIterator.NextSchObject;
+        End;
 
-                End;
-                Component.SchIterator_Destroy(ImplIterator);
+        Component.SchIterator_Destroy(ImplIterator);
 
-            ModelsList.Add('');
-//            // Send a system notification that component change in the library.
-//            SchServer.RobotManager.SendMessage(Component.I_ObjectAddress, c_BroadCast, SCHM_EndModify, c_NoEventData);
+        ModelsList.Add('');
 
+        SchServer.RobotManager.SendMessage(Component.I_ObjectAddress, c_BroadCast, SCHM_EndModify, c_NoEventData);
         Component.GraphicallyInvalidate;
         Component := Iterator.NextSchObject;
     End;
@@ -425,12 +450,10 @@ Begin
     CurrentLib.UpdateDisplayForCurrentSheet;
 
     SetDocumentDirty(true);
-    ModelsList.Insert(0, 'Count of FP Model totally removed/deleted : ' + IntToStr(FLinkCount));
-    GenerateReport(ModelsList, 'DataFileLinksRemovedFromCompLib.txt');
-    Modelslist.Free;
-
+    ModelsList.Add('Count of FP Model totally removed/deleted : ' + IntToStr(FLinkCount));
 End;
 
+{..............................................................................}
 Function GetAllSchCompParameters(const Component : ISch_BasicContainer) : TList;
 Var
    PIterator : ISch_Iterator;
@@ -450,4 +473,54 @@ Begin
     End;
     Component.SchIterator_Destroy( PIterator );
 End;
+{..............................................................................}
+Procedure GenerateReport(Report : TStringList, Filename : WideString);
+Var
+
+    ReportDocument : IServerDocument;
+    Filepath       : WideString;
+
+Begin
+    WS  := GetWorkspace;
+    If WS <> Nil Then
+    begin
+       Prj := WS.DM_FocusedProject;
+
+       Doc := WS.DM_FocusedDocument;
+
+       If Prj <> Nil Then
+          Filepath := ExtractFilePath(Prj.DM_ProjectFullPath);   //  Doc.DM_FullPath
+
+//  to get unique report file per SchLib doc..
+    //   Filepath := ExtractFilePath(Doc.FullPath) + ChangefileExt(ExtractFileName(Doc.FullPath), '');
+
+    end;
+
+    If length(Filepath) < 5 then Filepath := ExtractFilePath(Doc.DM_FullPath);
+
+    Filepath := Filepath + Filename;
+
+    ModelsList.Insert(0, ExtractFilename(Prj.DM_ProjectFullPath));
+    Report.SaveToFile(Filepath);
+
+    ReportDocument := Client.OpenDocument('Text',Filepath);
+    If ReportDocument <> Nil Then
+    begin
+        Client.ShowDocument(ReportDocument);
+        if (ReportDocument.GetIsShown <> 0 ) then
+            ReportDocument.DoFileLoad;
+    end;
+end;
+
+Procedure SetDocumentDirty (Dummy : Boolean);
+Var
+    AView           : IServerDocumentView;
+    AServerDocument : IServerDocument;
+Begin
+    If Client = Nil Then Exit;
+    AView := Client.GetCurrentView;
+    AServerDocument := AView.OwnerDocument;
+    AServerDocument.Modified := True;
+End;
+{..............................................................................}
 
